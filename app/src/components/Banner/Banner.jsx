347990c7a554875805d54d552f782ad837c84d7b
Boubamahir2 <amamahir1@gmail.com>
2023-05-17 01:30:13 +0200

    banner functionalities

--- app/src/components/Banner/Banner.jsx
+++ app/src/components/Banner/Banner.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
-
-const Banner = ({ imageSrc, bannerTitle }) => {
+import banner from '../../assets/banner.png';
+import bannerAbout from '../../assets/BannerAbout.png';
+import styles from './banner.module.css';
+function Banner(props) {
   return (
-    <div className='banner'>
-      <img src={imageSrc} alt='imageBanner' />
-      <h2>{bannerTitle}</h2>
+    <div className={styles.banner}>
+      {props.origin === 'home' ? (
+        <>
+          <img
+            className={styles.banner_img}
+            src={banner}
+            alt='Bannière paysage'
+          />
+          <div>
+            <h1 className={styles.banner_title}>
+              Chez vous, partout et ailleurs
+            </h1>
+          </div>
+        </>
+      ) : props.origin === 'about' ? (
+        <img
+          className={styles.banner_about_img}
+          src={bannerAbout}
+          alt='Bannière paysage'
+        />
+      ) : null}
     </div>
   );
-};
+}
 
-export default Banner
+export default Banner;
