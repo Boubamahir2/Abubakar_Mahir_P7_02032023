a027ec267990c5c5de9ad51d7168b4b0389a6f00
Boubamahir2 <amamahir1@gmail.com>
2023-05-17 01:42:41 +0200

    main layout setup

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import NotFound from './pages/NotFound/NotFound';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    
+    </BrowserRouter>
   )
 }
 
