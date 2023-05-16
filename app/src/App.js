3a14cd17f8cdd208f4923fba621c054fa5f500d0
Boubamahir2 <amamahir1@gmail.com>
2023-05-17 01:53:16 +0200

    home structure created

--- app/src/App.js
+++ app/src/App.js
@@ -4,14 +4,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import NotFound from './pages/NotFound/NotFound';
+import Appartment from './pages/Appartments/Appartment';
 
 
 const App = () => {
   return (
     <BrowserRouter>
-    
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/appartment/:id' element={<Appartment />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App
--- app/src/pages/Home/Home.jsx
+++ app/src/pages/Home/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Header from '../../components/Header/Header';
+import Banner from '../../components/Banner/Banner';
+import Gallery from '../../components/Gallary/Gallery';
+import Footer from '../../components/Footer/Footer';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <Header origin='home' />
+      <main>
+        <Banner origin='home' />
+        {/* <section className={styles.section_card}>
+          {Properties.map((element) => (
+            <Gallery key={element.id} element={element} />
+          ))}
+        </section> */}
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default Home
