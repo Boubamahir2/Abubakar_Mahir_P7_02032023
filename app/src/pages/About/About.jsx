f0b18a854f808e40cac3726b9849ae250249a485
Boubamahir2 <amamahir1@gmail.com>
2023-05-18 01:03:40 +0200

    About functionalities

--- app/src/pages/About/About.jsx
+++ app/src/pages/About/About.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Header from '../../components/Header/Header';
+import Banner from '../../components/Banner/Banner';
+import Footer from '../../components/Footer/Footer';
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <Header origin='about' />
+      <main>
+        <Banner origin='about' />
+        <div className="mainDropdown">
+          {
+            
+          }
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
-export default About
+export default About;
