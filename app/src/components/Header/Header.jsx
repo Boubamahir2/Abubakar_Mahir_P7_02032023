d94a17c02891740c925d07f1dc230d76198e7014
Boubamahir2 <amamahir1@gmail.com>
2023-05-17 02:10:00 +0200

    header components functionality added

--- app/src/components/Header/Header.jsx
+++ app/src/components/Header/Header.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom';
+import Logo from '../../assets/logo-header.png';
+import './Header.css';
 
-const Header = () => {
+const Header = (props) => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className='_header'>
+      <nav className='header_container'>
+        <NavLink to='/'>
+          <img className='header_logo' src={Logo} alt='Logo Kasa' />
+        </NavLink>
+        <div className='header_nav'>
+          <NavLink
+            className={
+              props.origin === 'home'
+                ? 'header_nav_underline'
+                : 'header_nav_empty'
+            }
+            to='/'
+          >
+            Accueil
+          </NavLink>
+          <NavLink
+            className={
+              props.origin === 'about'
+                ? 'header_nav_underline'
+                : 'header_nav_empty'
+            }
+            to='/about'
+          >
+            A propos
+          </NavLink>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
