6dcfd3d98fe893bbcc6f6386e5e64b3a91d65f79
Boubamahir2 <amamahir1@gmail.com>
2023-05-17 23:55:48 +0200

    footer component

--- app/src/components/Footer/Footer.jsx
+++ app/src/components/Footer/Footer.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import './footer.css'
+import footerLogo from '../../assets/logo-footer.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <div className='footer'>
+        <img className='footer_logo' src={footerLogo} alt='Logo Kasa' />
+        <p className='footer_text'>© 2020 Kasa. All rights reserved</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer
