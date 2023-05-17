64b4a2710fffc5591729f3525691e4c09a675955
Boubamahir2 <amamahir1@gmail.com>
2023-05-18 01:41:39 +0200

    Dropdown jsx skeleton

--- app/src/components/Dropdown/Dropdown.jsx
+++ app/src/components/Dropdown/Dropdown.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
+import arrowDown from '../../assets/svg/dow-arrow.svg';
+import arrowUp from '../../assets/arrowUp.png';
 
-const Dropdown = () => {
+const Dropdown = ({ title, text_content }) => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>Dropdown</div>
-  )
-}
+    <div className='dropdown'>
+      <div className='dropdown_title_arrow'>
+        <h3 className='dropdown_title'>{title}</h3>
+        <img
+          className='dropdown_arrow'
+          src={isOpen ? arrowUp : arrowDown}
+          alt='flèche'
+          onClick={() => setIsOpen(!isOpen)}
+        />
+      </div>
+      {isOpen && (
+        <div>
+          <p className='dropdown_content'>{text_content}</p>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Dropdown
