d75208e2ed41b6c577601a40a41aa1c59e69823e
Boubamahir2 <amamahir1@gmail.com>
2023-05-18 00:16:53 +0200

    not found component added

--- app/src/pages/NotFound/NotFound.jsx
+++ app/src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import Header from '../../components/Header/Header';
+import { NavLink } from 'react-router-dom';
+import './error.css'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div>
+      <Header origin='error' />
+      <div className='error_section'>
+        <div className='error_title'>
+          <h1 className='error_h1'>404</h1>
+          <p className='error_p'>
+            Oups! La page que <span>vous demandez n'existe pas</span>.
+          </p>
+          <NavLink
+            className='error_back_home'
+            to='/'
+            title="Retour page d'accueil"
+          >
+            Retourner sur la page d'accueil
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
