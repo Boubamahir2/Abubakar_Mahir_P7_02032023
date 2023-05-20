faa3a91feac1642a99a1113f1716fee2da27d4ce
Boubamahir2 <amamahir1@gmail.com>
2023-05-20 10:33:50 +0200

    lodging styles

--- app/src/pages/Lodging/Lodging.jsx
+++ app/src/pages/Lodging/Lodging.jsx
@@ -1,3 +1,4 @@
+import './Lodging.css';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import Carousel from '../../components/Carousel/Carousel';
