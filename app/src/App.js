22220b6873da44dcb94f3528b7c33ef62e03011c
Boubamahir2 <amamahir1@gmail.com>
2023-05-20 02:18:12 +0200

    Lodging folder added

--- app/src/App.js
+++ app/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import NotFound from './pages/NotFound/NotFound';
-import Appartment from './pages/Appartments/Appartment';
+import Appartment from './pages/Appartment/Appartment';
 
 
 const App = () => {
