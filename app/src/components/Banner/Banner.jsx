db855573bbdfc72a8cf0a71b71d08dff47547b3d
Boubamahir2 <amamahir1@gmail.com>
2023-05-28 02:55:28 +0200

    banner component modified

--- app/src/components/Banner/Banner.jsx
+++ app/src/components/Banner/Banner.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import banner from '../../assets/images/banner.png';
+import bannerAbout from '../../assets/images/BannerAbout.png';
 
-function Banner({ imageSrc, bannerTitle }) {
-  return (
-    <div className="banner">
-        <img src={imageSrc} alt="imageBanner" />
-      <h2>{bannerTitle}</h2>
+function Banner(props) {
+  return props.origin === 'home' ? (
+    <div className='banner'>
+      <img className='banner_img' src={banner} alt='Bannière paysage' />
+      <h2 className='banner_title'>Chez vous, partout et ailleurs</h2>
+    </div>
+  ) : (
+    <div className='banner'>
+      <img className='banner_img' src={bannerAbout} alt='Bannière paysage' />
+      <h2 className='banner_title'>Chez vous, partout et ailleurs</h2>
     </div>
   );
 }
--- app/src/pages/Home/Home.jsx
+++ app/src/pages/Home/Home.jsx
@@ -1,20 +1,20 @@
 import Banner from "../../components/Banner/Banner";
 import Card from "../../components/Card/Card";
 
-function Home({apparts}) {
-  const bannerTitle = 'Chez vous, partout et ailleurs'
-  const imageIndex = 1;
-  console.log(apparts)
 
+function Home({apparts}) {
   return (
-    <div className="home">
-      <Banner imageSrc={`./banner${imageIndex}.jpg`} bannerTitle={bannerTitle}/>
-      <div className="cards">
-       {
-        apparts.map((appart) => (
-          <Card key={appart.id} appartId={appart.id} appartTitle={appart.title} appartCover={appart.cover} />
-        ))
-       }
+    <div className='home'>
+      <Banner origin='home' />
+      <div className='cards'>
+        {apparts.map((appart) => (
+          <Card
+            key={appart.id}
+            appartId={appart.id}
+            appartTitle={appart.title}
+            appartCover={appart.cover}
+          />
+        ))}
       </div>
     </div>
   );
