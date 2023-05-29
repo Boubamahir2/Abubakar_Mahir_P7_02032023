1fe7aad630c16f59edaff5b0f62c0ce33c955143
Boubamahir2 <amamahir1@gmail.com>
2023-05-29 12:18:44 +0200

    end point modified

--- app/src/components/Card.jsx
+++ app/src/components/Card.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
 
-function Card({appartId, appartTitle, appartCover }) {
+import { Link } from 'react-router-dom';
 
-  
+function Card({ appartId, appartTitle, appartCover }) {
+
   return (
-    <Link className="card" to={'../lodging/'+ appartId } >
-      <div className="cardImg">
+    <Link className='card' to={`/lodging/${appartId}`}>
+      <div className='cardImg'>
         <img src={appartCover} alt={appartTitle} />
       </div>
       <h4>{appartTitle}</h4>
